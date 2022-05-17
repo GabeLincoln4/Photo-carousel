99f79d20c09acd654fb6ba18138171b9aea1924f
Gavin <7797864896@K12.LCS>
2022-05-17 16:50:52 -0400

    Change default hello world boilerplate code

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <photo-carousel></photo-carousel>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PhotoCarousel from './components/carousel.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PhotoCarousel
   }
 }
 </script>
---
+++ src/components/carousel.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Photo Carousel</h1>
+</template>
+
+<script>
+export default {
+  name: 'PhotoCarousel',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style>
+
+</style>
